12029496781425b7af6af3f252b7c596bcca0604
peteciank <106826936+peteciank@users.noreply.github.com>
2025-05-04 22:48:27 -0300

    Badge approach

--- Home.py
+++ Home.py
@@ -12,6 +12,9 @@ with st.sidebar:
                 width: 400px !important; # Set the width to your desired value
             }
         </style>
+        <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+        <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="pedrociancaglini" data-version="v1">
+        <a class="badge-base__link LI-simple-link" href="https://es.linkedin.com/in/pedrociancaglini/en?trk=profile-badge"></a></div>
         """,
         unsafe_allow_html=True,
     )
