7583d4fcbdd8d52396e7e127a9c595114a38226b
peteciank <106826936+peteciank@users.noreply.github.com>
2025-05-04 22:45:26 -0300

    new home

--- Home.py
+++ Home.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
 st.title("This is My Streamlit Lab, this is my kindom come 😂")
+
+with st.sidebar:
+    st.markdown(
+        """
+        <style>
+            section[data-testid="stSidebar"] {
+                width: 400px !important; # Set the width to your desired value
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    components.html(
+        """
+        <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+        <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="pedrociancaglini" data-version="v1">
+        <a class="badge-base__link LI-simple-link" href="https://es.linkedin.com/in/pedrociancaglini/en?trk=profile-badge"></a></div>
+        """,
+    height=320,
+    )
