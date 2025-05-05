9916c4b4e0a796eb178121588d6948f818a76e26
peteciank <106826936+peteciank@users.noreply.github.com>
2025-05-05 17:54:13 -0300

    .

--- pages/db/bi/src/app.py
+++ pages/db/bi/src/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from static.utils import create_connection
+from lib.utils import create_connection
+
 
 # make sure the db exists irrespective of where user comes from
 create_connection('core.db')
