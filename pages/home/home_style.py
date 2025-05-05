cc5ed20918fce7467412c8b169c309dfd512de45
peteciank <106826936+peteciank@users.noreply.github.com>
2025-05-05 16:53:54 -0300

    style metrics

--- pages/home/home_style.py
+++ pages/home/home_style.py
@@ -30,3 +30,12 @@ st.markdown("""
 # Sidebar configuration
 st.sidebar.title("Sidebar")
 st.sidebar.write("This is the sidebar content.")
+
+
+with open('style_metrics.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
