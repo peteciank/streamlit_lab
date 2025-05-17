b75938748cbebf0aef0a88c9fe957bf68333f580
peteciank <106826936+peteciank@users.noreply.github.com>
2025-05-17 13:11:03 -0300

    context in menu

--- Home.py
+++ Home.py
@@ -25,6 +25,8 @@ db_save = st.Page("pages/db/bi/src/pages/save_query.py", title="Save Query", ico
 data_app = st.Page("pages/styling/superapp.py", title="Data Management", icon=":material/database:")
 counter = st.Page("pages/styling/counter.py", title="DB", icon=":material/database:")
 
+# ----------- Research -------------------------
+context = st.Page("pages/Context.py", title="Context", icon=":material/keyboard:")
 
 
 # ---------- NAVIGATION ----------
@@ -33,7 +35,7 @@ pg = st.navigation(
         "Home": [home],
         "Organizers": [col_size],
         "DB": [db_start, db_api, db_call_api, db_upload, db_delete_query, db_delete_table, db_plot, db_save],
-        #"Styling": [data_app, counter],
+        "Research": [context],
     }, position="sidebar"
 )
 
