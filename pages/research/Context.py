34a022c6c452a0aee3f3e33fea720a3317714740
peteciank <106826936+peteciank@users.noreply.github.com>
2025-06-30 09:37:33 -0300

    using page link

--- pages/research/Context.py
+++ pages/research/Context.py
@@ -57,6 +57,8 @@ f"The user's local time is {now.astimezone(tz_obj_delta)}"
 if st.context.url.startswith("http://localhost"):
     st.write("You are running the app locally.")
 
+st.page_link("Home.py", label="Home", icon="🏠")
+
 
 pg = st.navigation([st.Page("pages/home/home_style.py")])
 pg.run()
